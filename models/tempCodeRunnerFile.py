78f38d9a17d687f94908eb2fd3134b1e13274bd5
Daniel Filipe <251019771@aluno.unb.br>
2025-11-27 12:42:39 -0300

    organizaçao

--- models/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    # método auxiliar para mexer peças no board temporário
-    def setPieceOn(self, board, position, piece):
-
-        col = ord(position[0]) - ord('a')
-        row = 8 - int(position[1])
-        board.game[row][col] = piece
