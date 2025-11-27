e759bc9ef97d0964c2c069e8f24850eba3e83821
nbg-cordeiro <251023184@aluno.unb.br>
2025-11-27 18:47:43 -0300

    Adiciona método para criar perfis

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,7 @@
 from bottle import Bottle, request
 from .base_controller import BaseController
 from services.user_service import UserService
+from services.game_service import GameService
 
 class UserController(BaseController):
     def __init__(self, app):
@@ -8,14 +9,15 @@ class UserController(BaseController):
 
         self.setup_routes()
         self.user_service = UserService()
+        self.game_service = GameService()
 
 
-    # Rotas User
     def setup_routes(self):
         self.app.route('/users', method='GET', callback=self.list_users)
         self.app.route('/users/add', method=['GET', 'POST'], callback=self.add_user)
         self.app.route('/users/edit/<user_id:int>', method=['GET', 'POST'], callback=self.edit_user)
         self.app.route('/users/delete/<user_id:int>', method='POST', callback=self.delete_user)
+        self.app.route('/profile/<user_id:int>', method=['GET', 'POST'], callback=self.profile)
 
 
     def list_users(self):
@@ -48,6 +50,20 @@ class UserController(BaseController):
     def delete_user(self, user_id):
         self.user_service.delete_user(user_id)
         self.redirect('/users')
+
+    
+    def profile(self, user_id):
+        user = self.user_service.get_by_id(user_id)
+        if not user:
+            return "Usuário não encontrado"
+
+        if request.method == 'POST':
+            self.user_service.edit_user(user)
+            return self.redirect(f'/profile/{user_id}')
+
+        games = self.game_service.get_games_by_player(user.name)
+        
+        return self.render('profile', user=user, games=games)
 
 
 user_routes = Bottle()
